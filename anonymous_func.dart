416c83baf42e160d97cb6fcbd4f3ef03c6ec7ef7
Okumu Joseph <jokumu63@yahoo.com>
2024-03-13 14:01:17 +0300

    anonymous functions

---
+++ anonymous_func.dart
@@ -0,0 +1,20 @@
+
+
+// void main() {
+//   const fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+//   fruits.forEach((fruit) {
+//     print(fruit);
+//   });
+// }
+
+
+void main() {
+  // list of cars
+  List cars = ["BMW", "BENZ", "AUDI", "TOYOTA"];
+
+  // iteration with anonymous function as a parameter
+  cars.forEach((car) {
+    print(car); // printing an item
+  });
+}
